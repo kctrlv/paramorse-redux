213a736c84ed05072098b9048e4003923ebae6f9
Ryan Workman <rdavid1099@gmail.com>
2016-07-11 16:17:05 -0600

    Add auto-detect functionality

--- lib/stream_decoder.rb
+++ lib/stream_decoder.rb
@@ -1,48 +1,55 @@
 require '../lib/letter_decoder'
-# require '../lib/letter_encoder'
 require '../lib/morse_queue'
+require 'pry'
 
 class StreamDecoder
+  attr_reader :letter_string
+
   def initialize
     @queue = MorseQueue.new
+    @letter_string = String.new
     @decode = LetterDecoder.new
     @detect_end_of_letter = String.new
     @input = String.new
-    @complete_letter = Array.new
+    @complete_letter = String.new
   end
 
   def receive(input)
     @queue.push(input)
     detect_end_of_letter(input) if input == '0'
     clear_end_of_letter if input == '1'
-    @queue
   end
 
   def detect_end_of_letter(input)
     @detect_end_of_letter += input
-    if @detect_end_of_letter =~ /000/
-      @complete_letter << @queue.pop until @queue.count == 0
-    end
-    @detect_end_of_letter
+    end_of_letter if @detect_end_of_letter =~ /000/ && !end_of_word?
+    end_of_word if end_of_word?
+  end
+
+  def end_of_letter
+  @complete_letter += @queue.pop until @queue.count == 0
+  decode
+  clear_complete_letter
+  end
+
+  def end_of_word
+
+  end
+
+  def end_of_word?
+    return true if @detect_end_of_letter =~ /000000/
+    false
   end
 
   def clear_end_of_letter
     @detect_end_of_letter = ""
   end
 
+  def clear_complete_letter
+    @complete_letter = ""
+  end
+
   def decode
-    @decode.decode(send_complete_letter)
+    @letter_string += @decode.decode(@complete_letter)
   end
-
-  def send_complete_letter
-    @complete_letter.join.gsub(/000/,"")
-  end
-
-  # def encode
-  #   until @queue.count == 0
-  #     @input += @queue.pop
-  #   end
-  #   @encode.encode(@input)
-  # end
-
 end
--- test/stream_test.rb
+++ test/stream_test.rb
@@ -10,15 +10,14 @@ class TestStream < Minitest::Test
     assert_instance_of StreamDecoder, @stream
   end
   def test_stream_can_receive_information
-    assert @stream.receive('1')
-  end
-  def test_stream_can_receive_two_pieces_of_data
-    assert @stream.receive('1')
-    assert @stream.receive('0')
+    assert_respond_to @stream, :receive
   end
   def test_stream_can_decode_data_received
     @stream.receive('1')
-    assert_equal 'e', @stream.decode
+    @stream.receive('0')
+    @stream.receive('0')
+    @stream.receive('0')
+    assert_equal 'e', @stream.letter_string
   end
   def test_stream_can_decode_more_than_one_item_received
     @stream.receive('1')
@@ -26,7 +25,10 @@ class TestStream < Minitest::Test
     @stream.receive('1')
     @stream.receive('1')
     @stream.receive('1')
-    assert_equal 'a', @stream.decode
+    @stream.receive('0')
+    @stream.receive('0')
+    @stream.receive('0')
+    assert_equal 'a', @stream.letter_string
   end
   def test_stream_can_decode_word_streamed
     @stream.receive('1')
@@ -44,14 +46,17 @@ class TestStream < Minitest::Test
     @stream.receive('1')
     @stream.receive('1')
     @stream.receive('1')
-    assert_equal 'ha', @stream.decode
-  end
-  def test_stream_detects_zeroes
-    assert @stream.detect_end_of_letter('0')
-  end
-  def test_stream_receive_detects_zeroes
     @stream.receive('0')
-    assert_equal '00', @stream.detect_end_of_letter('0')
+    @stream.receive('0')
+    @stream.receive('0')
+    assert_equal 'ha', @stream.letter_string
+  end
+  def test_stream_receive_detects_letter
+    @stream.receive('1')
+    @stream.receive('0')
+    @stream.receive('0')
+    @stream.receive('0')
+    assert_equal 'e', @stream.letter_string
   end
   def test_stream_automatically_dumps_letters
     @stream.receive('1')
@@ -64,6 +69,6 @@ class TestStream < Minitest::Test
     @stream.receive('0')
     @stream.receive('0')
     @stream.receive('0')
-    assert_equal '1010101', @stream.send_complete_letter
+    assert_equal 'h', @stream.letter_string
   end
 end
